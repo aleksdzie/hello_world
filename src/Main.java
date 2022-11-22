b22ae8471c3b6fb8ffd86dd7b0e3de9428999e16
aleksdzie <aleksandra.dziedzic1@gmail.com>
2022-11-22 20:14:28 +0100

    Przedstawienie się

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String name="Aleksandra";
+        int age=32;
+        System.out.println("Cześć, mam na imię "+name+" i mam "+age+" lata");
     }
 }
