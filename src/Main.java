0c777bcf427827826094f0d25c228660c839062a
aleksdzie <aleksandra.dziedzic1@gmail.com>
2022-11-22 20:31:05 +0100

    Dodanie zapytań o imię i wiek

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        String name="Aleksandra";
-        int age=32;
-        System.out.println("Cześć, mam na imię "+name+" i mam "+age+" lata");
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Wprowadź imię.");
+        String name=scanner.nextLine();
+        System.out.println("Wprowadź swój wiek.");
+        int age=scanner.nextInt();
+        System.out.println("Cześć, mam na imię "+name+" i mam "+age+" lat(a)");
     }
 }
